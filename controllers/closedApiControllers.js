ae96345350cdf66c8cf7e6d70939db527f42d361
IvanB475 <ivanbeljan@net.hr>
2021-04-26 17:42:11 +0200

    fixed validateToken api

--- controllers/closedApiControllers.js
+++ controllers/closedApiControllers.js
@@ -2,10 +2,11 @@ const validateTokenFunc = require("../utils/validateToken");
 
 exports.validateToken = async (req, res, next) => {
   const token = req.header("Authorization");
-  console.log("token from header is" + token);
-  const validated = validateTokenFunc.validateToken(token);
+  console.log(token);
+  const validated = await validateTokenFunc.validateToken(token);
   if (validated) {
-    next();
+    console.log("validated" + validated);
+    res.status(200).json({ valid: "OK" });
   } else {
     res.status(403).json({ message: "You are not authorized to use this API" });
   }
--- utils/validateToken.js
+++ utils/validateToken.js
@@ -3,8 +3,7 @@ const jwt = require("jsonwebtoken");
 exports.validateToken = async (token) => {
   try {
     const tokenSecret = process.env.TOKEN_SECRET;
-    const token = jwt.verify(token, tokenSecret);
-
+    const validationResult = jwt.verify(token, tokenSecret);
     return true;
   } catch (e) {
     return false;
